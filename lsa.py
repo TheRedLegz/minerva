d412f603273ecf244150aa703adac6c8cc0cf717
John Paul Lopez <jpblopez05@gmail.com>
2021-07-24 21:43:16 +0800

    adding pca and some modification to lsa 😳

--- lsa.py
+++ lsa.py
@@ -22,6 +22,7 @@ import math
 
 tweets_df = pd.read_json (r'sample.json')
 
+
 def preprocessing():
     # stop_words = stopwords.words('english')
     tweets_df['clean_doc'] = tweets_df['full_text'].apply(lambda x: re.split('https:\/\/.*', str(x))[0])
@@ -38,46 +39,54 @@ def preprocessing():
     tweets_df['clean_doc'] = detokenized_doc
     return detokenized_doc
 
-def topicModeling(detokenized):
-    vectorizer = TfidfVectorizer(ngram_range=(1,2),stop_words='english', 
-    max_features= 1000, # keep top 1000 terms 
-    max_df = 0.5, 
-    smooth_idf=True)
-    X = vectorizer.fit_transform(detokenized)
-    # print(X.shape)
+# def topicModeling(detokenized):
+#     vectorizer = TfidfVectorizer(ngram_range=(1,2),stop_words='english', 
+#     max_features= 1000, # keep top 1000 terms 
+#     max_df = 0.5, 
+#     smooth_idf=True)
+#     X = vectorizer.fit_transform(detokenized)
+#     # print(X.shape)
 
-    svd_model = TruncatedSVD(n_components=20, algorithm='randomized', n_iter=100, random_state=122)
-    svd_model.fit_transform(X)
-    # terms = vectorizer.get_feature_names()
+#     svd_model = TruncatedSVD(n_components=20, algorithm='randomized', n_iter=100, random_state=122)
+#     svd_model.fit_transform(X)
+#     # terms = vectorizer.get_feature_names()
 
-    # print(len(terms))
-    # for i, comp in enumerate(svd_model.components_):
-    #     terms_comp = zip(terms, comp)
-    #     sorted_terms = sorted(terms_comp, key= lambda x:x[1], reverse=False)[:7]
-    #     print("Topic "+str(i)+": ")
-    #     for t in sorted_terms:
-    #         print(t)
-    #         print(" ")
-    #     print(sorted_terms)
-    pprint(svd_model.components_)
-    print(len(svd_model.components_))
-    transpose = numpy.transpose(svd_model.components_)
-    # pprint(transpose)
-    scaler = MinMaxScaler()
-    scaled = scaler.fit_transform(transpose)
+#     # print(len(terms))
+#     # for i, comp in enumerate(svd_model.components_):
+#     #     terms_comp = zip(terms, comp)
+#     #     sorted_terms = sorted(terms_comp, key= lambda x:x[1], reverse=False)[:7]
+#     #     print("Topic "+str(i)+": ")
+#     #     for t in sorted_terms:
+#     #         print(t)
+#     #         print(" ")
+#     #     print(sorted_terms)
+#     pprint(svd_model.components_)
+#     print(len(svd_model.components_))
+#     transpose = numpy.transpose(svd_model.components_)
+#     # pprint(transpose)
+#     scaler = MinMaxScaler()
+#     scaled = scaler.fit_transform(transpose)
 
-    # pca = PCA(n_components = 2)
-    # principalComponents = pca.fit_transform(svd_model.components_)
-    # principalDf = pd.DataFrame(data = principalComponents, columns = ['1', '2'])
+#     # pca = PCA(n_components = 2)
+#     # principalComponents = pca.fit_transform(svd_model.components_)
+#     # principalDf = pd.DataFrame(data = principalComponents, columns = ['1', '2'])
     
-    return transpose
+#     return transpose
 
 def lsiGensim(detokenized):
     processed_corpus = preprocess_documents(detokenized)
+
+    # do bigram
+    # input bigram into the dictionary
+
     dictionary = Dictionary(processed_corpus)
+
     bow_corpus = [dictionary.doc2bow(text) for text in processed_corpus]
     tfidf = TfidfModel(bow_corpus, smartirs='npu')
+
     corpus_tfidf = tfidf[bow_corpus]
+
+
     coherenceList_UMass = []
     numTopicsList = [35,36,37,38,39,40]
     for k in numTopicsList:
@@ -85,20 +94,65 @@ def lsiGensim(detokenized):
         coherenceList_UMass.append(c_UMass)
     plt.plot(numTopicsList, coherenceList_UMass)
     minpos = coherenceList_UMass.index(min(coherenceList_UMass))
+
+
     optimized = LsiModel(corpus=corpus_tfidf, num_topics=numTopicsList[minpos])
-    print(optimized.get_topics())
-    plt.show()
-    return optimized.get_topics()
+
+    # research y 22 topics are showing, instead of 30+
+    # try to examine the tfidf matrix first
+    # 
+
+    topics = optimized.get_topics()
+    return topics
     
 
 def compute_coherence_UMass(corpus, dictionary, k):
-   lsi_model = LsiModel(corpus=corpus, num_topics=k)
-   coherence = CoherenceModel(model=lsi_model,
-                              corpus=corpus,
-                              dictionary=dictionary,
-                              coherence='u_mass')
-   return coherence.get_coherence()
+    lsi_model = LsiModel(corpus=corpus, num_topics=k)
+    coherence = CoherenceModel(model=lsi_model,corpus=corpus, dictionary=dictionary,coherence='u_mass')
+
+    return coherence.get_coherence()
+
+
+
+
+def pca(matrix, k):
+    # TODO must reduce the number of topics
+
+    # the input must be words x topics
+    # k must be the desired num of topics
+
+    # ? maybe scale the things first before doing the PCA
+    
+
+    print(matrix.shape)
+    scaler = MinMaxScaler()
+
+    scaled = scaler.fit_transform(matrix)
+
+    
+
+    pca = PCA(k)
+
+    res = pca.fit_transform(scaled)
+
+
+    print(pca.explained_variance_ratio_.cumsum())
+    print(numpy.array(res).shape)
+    return res
+
+
 
 
 texts = preprocessing()
-lsiGensim(texts)
+
+
+something = [
+    "cat cat cat cat",
+    "cat some cat some",
+    "cute cat haha"
+]
+
+
+matrix = numpy.array(lsiGensim(texts)).T
+
+res = pca(matrix, 16)
