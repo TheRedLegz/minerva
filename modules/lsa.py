00d0e9d317f161b41217097841d55cd117ec1fe3
John Paul Lopez <jpblopez05@gmail.com>
2021-07-30 21:04:23 +0800

    modified bow to use both uni and bigram

--- modules/lsa.py
+++ modules/lsa.py
@@ -1,13 +1,15 @@
+import nltk
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+
 import math
 import numpy as np
 from pprint import pprint
-import nltk
 import re
 import string as str
 from nltk.stem import WordNetLemmatizer
-from numpy.lib import unique
 
-nltk.download('stopwords')
 from nltk.corpus import stopwords
 
 
@@ -20,69 +22,82 @@ def preprocess(string):
         res = re.sub(r'\d+', '', string)
         return res
 
-
     def remove_punctation(string):
-        res = string.translate(str.maketrans("", ""), str.punctuation)
+        res = string.translate(string.maketrans("", "", str.punctuation))
         return res
 
+    # NOTE this returns an array
     def remove_stop_words(string):
-        res = [w for w in string if w not in stop_words]
+        split = string.split(' ')
+        res = [w for w in split if w not in stop_words]
         return res
 
+    def lemmatize_string(string):
+        split = string
+
+        if not isinstance(string, list):    
+            split = string.split(' ')
+
+        res = []
+
+        for word in split:
+            lemma = lemmatizer.lemmatize(word)
+            res.append(lemma)
+
+        return ' '.join(res)
 
     string = string.lower().strip()
     string = remove_numbers(string)
     string = remove_punctation(string)
     string = remove_stop_words(string)
-    string = lemmatizer.lemmatize(string)
+    string = lemmatize_string(string)
 
     return string
 
-
 def bag_of_words(document_array):
 
-
-    document_bigrams = []
+    doc_grams = []
 
     for doc in document_array:
-        tokenized = nltk.word_tokenize(doc)
-        grammed = nltk.bigrams(tokenized)
-        grammed = map(lambda x: x[0] + '_' + x[1], grammed)
-        document_bigrams.append(list(grammed))
+        string = preprocess(doc)
+        unigrams = nltk.word_tokenize(string)
+        bigrams = nltk.bigrams(unigrams)
+        bigrams = map(lambda x: x[0] + '_' + x[1], bigrams)
+        uni_bi_grams = list(bigrams) + unigrams
+        doc_grams.append(list(uni_bi_grams))
 
 
-    unique_bigrams = []
+    unique_grams = []
 
-    for doc in document_bigrams:
+    for doc in doc_grams:
         for bigram in doc:
-            if bigram not in unique_bigrams:
-                unique_bigrams.append(bigram)
-
+            if bigram not in unique_grams:
+                unique_grams.append(bigram)
+    
 
     # doc = row, bigram = col
-    document_count = len(document_bigrams)
-    bigram_count = len(unique_bigrams)
+    document_count = len(doc_grams)
+    gram_count = len(unique_grams)
 
-    bow_bigrams = np.zeros((document_count, bigram_count), dtype=int)
+    bow_grams = np.zeros((document_count, gram_count), dtype=int)
     
 
     for i in range(document_count):
-        doc = document_bigrams[i]
+        doc = doc_grams[i]
 
-        for j in range(bigram_count):
-            bigram = unique_bigrams[j]
-            count = doc.count(bigram)
+        for j in range(gram_count):
+            gram = unique_grams[j]
+            count = doc.count(gram)
 
-            bow_bigrams[i][j] = int(count)
+            bow_grams[i][j] = int(count)
 
-    
-    return (bow_bigrams, unique_bigrams)
+    return (bow_grams, unique_grams, doc_grams)
 
 
 def tf_idf(document_array, bow = None):
 
     if bow is None:
-        (bow, _) = bag_of_words(document_array)
+        (bow, x, y) = bag_of_words(document_array)
 
     matrix = np.zeros(bow.shape, dtype=float)
     doc_count = len(document_array)
@@ -92,8 +107,9 @@ def tf_idf(document_array, bow = None):
         word_count = np.sum(row)
 
         for j, col in enumerate(row):
+            
             tf = col / word_count
-
+        
             has_word_count = np.count_nonzero(bow[:, j:j+1])
             idf = math.log(doc_count / (has_word_count + 1))
 
@@ -119,4 +135,4 @@ data = [
 ]
 
 
-tf_idf(data)
+pprint(tf_idf(data))
