f2e0c592dca326cc82d08c9c0313f11188bd2caa
Redempto D. Legaspi III <redempto.legaspi.20@usjr.edu.ph>
2021-08-09 23:17:09 +0800

    Removed unused files

--- vectorizer.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-def vectorize_text(text):
-    vectorizer = TfidfVectorizer(stop_words='english', 
-    max_features= 1000, # keep top 1000 terms 
-    max_df = 0.5, 
-    smooth_idf=True)
-
-    return vectorizer.fit_transform(text)
