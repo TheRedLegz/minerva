e66e494dbc4ed350a3e44fdb096009ee1ab35ff5
Redempto D. Legaspi III <redempto.legaspi.20@usjr.edu.ph>
2021-08-11 06:38:38 +0800

    Updated SOM_methods

--- som_methods.py
+++ som_methods.py
@@ -219,3 +219,133 @@ def pca(matrix):
     res = pca_final.fit_transform(scaled)
 
     return res
+
+def most_common(lst, n):
+  if len(lst) == 0:
+      return -1
+      
+  counts = np.zeros(shape=n, dtype=np.int)
+
+  for i in range(len(lst)):
+    counts[lst[i]] += 1
+
+  return np.argmax(counts)
+
+def find_bmu(matrix, input, matrix_size):
+    (row, col) = matrix_size
+
+    num_features = len(input)
+
+    bmu = (0,0)
+    nearest = 10000000
+
+    for i in range(row):
+        for j in range(col):
+
+            distance = euc_distance(input, matrix[i][j], num_features)
+
+            if distance < nearest:
+                nearest = distance
+                bmu = (i, j)
+
+    return bmu
+
+def euc_distance(input, cell, num_features):
+    sum = 0
+
+    for n in range(num_features):
+        sum = sum + pow(input[n] - cell[n], 2)
+    
+    return math.sqrt(sum)
+
+def man_distance(bmu_row, bmu_col, row, col):
+    return np.abs(bmu_row-row) + np.abs(bmu_col-col)
+
+def SOM(data, learn_rate, matrix_size):
+
+    # 1 Initialize some constants
+
+    (steps, num_features) = data.shape
+    (row, col) = matrix_size
+    range_max = row + col 
+
+    # 2 Create the initial matrix
+
+    matrix = np.random.random_sample(size=(row,col,num_features))
+
+    # 3 Run main logic
+
+    steps_max = 5000 
+
+    indices = np.zeros(len(data))
+
+    for s in range(steps_max):
+        if s % (steps_max/50) == 0: print(str((s/steps_max) * 100.00) + " percent")
+        
+        # Update
+        percent = 1.0 - ((s * 1.0) / steps_max)
+
+        # curr_range for manhattan distance updating
+        curr_range = (int)(percent * range_max)
+        
+        # curr_range for pythagorean updating
+        # curr_range = range_max * exp(-(s / steps_max))
+        curr_rate = percent * learn_rate
+
+        # get a unique input from data
+
+        rand = np.random.randint(len(data))
+
+        input = data[rand]
+
+        # get the bmu
+        (bmu_row, bmu_col) = find_bmu(matrix, input, matrix_size)
+
+        for i in range(row):
+            for j in range(col):
+
+                cell = matrix[i][j]
+                
+                # Weight Updating (w/ man_distance)
+                if man_distance(bmu_row, bmu_col, i, j) < curr_range:
+                    matrix[i][j] = cell + curr_rate * (input-cell)
+
+                    # [1, 0.4, 0.5, ..., 9n]
+                    # [0.3, 0.6, 1, ..., 9n]
+
+                # Weight Updating (w/o man_distance)
+                # Formula: cell+curr_rate*(EXP(-((POWER(bmui-i,2)+POWER(bmuj-j,2))/(num_features???*POWER(curr_range,2)))))*(input- cell)
+                # matrix[i][j] = cell  + curr_rate * (exp(-(((bmu_row - i)**2 + (bmu_col - j)**2)/(num_features*(curr_range**2))))) * (input - cell)
+
+    return matrix
+
+def construct_u_matrix(SOM_matrix, matrix_size):
+
+    (row, col) = matrix_size
+    u_matrix = np.zeros(shape=matrix_size, dtype=np.float64)
+
+    for i in range(row):
+        for j in range(col):
+            
+            v = SOM_matrix[i][j]
+
+            sum_dists = 0.0
+            ct = 0
+            
+            if i-1 >= 0:  
+                sum_dists += euc_distance(v, SOM_matrix[i-1][j], len(v)); ct += 1
+
+            if i+1 <= row-1:
+                sum_dists += euc_distance(v, SOM_matrix[i+1][j], len(v)); ct += 1
+
+            if j-1 >= 0:  
+                sum_dists += euc_distance(v, SOM_matrix[i][j-1], len(v)); ct += 1
+
+            if j+1 <= col-1: 
+                sum_dists += euc_distance(v, SOM_matrix[i][j+1], len(v)); ct += 1
+
+            
+            u_matrix[i][j] = sum_dists / ct
+
+    plt.imshow(u_matrix, cmap='gray') 
+    plt.show()
