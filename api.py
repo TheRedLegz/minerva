6bdbaaaa8f93f72d5af10b64621c53fe2d6aa064
John Paul Lopez <jpblopez05@gmail.com>
2021-10-21 17:03:08 +0800

    making preprocessing api partial

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, abort
 from pymongo import MongoClient
 from flask_cors import CORS
-
+from modules.tweet_preprocessor import basic_clean
+from modules.gram import gram_documents
 
 app = Flask(__name__)
 CORS(app)
@@ -37,6 +38,38 @@ def get_one_tweet(tweet_id):
 
     return error
     
+
+@app.route('/preprocessing', methods=['GET'])
+def get_processing_data():
+    db_results = list(rawtweets.find())
+    data = []
+
+    for a in db_results:
+        to_add = {}
+        to_add['full_text'] =  a['data']['full_text']
+        to_add['created_at'] =  a['data']['created_at']
+        to_add['id'] =  a['data']['id']
+
+        to_add['cleaned'] = basic_clean(a['data']['full_text'])
+
+        data.append(to_add)
+
+
+    return jsonify({ 'data': data })
+
+
+@app.route('/grams', methods=['GET'])
+def get_grams():
+    db_results = list(rawtweets.find())
+    data = []
+
+    for a in db_results:
+        data.append(a['data']['full_text'])
+
+    tokens = gram_documents(data)
+
+    return jsonify({ 'data': tokens })
+
 
 if __name__ == '__main__':
     app.run(debug=True)
