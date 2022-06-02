04a6aeb69399f3ea84b2719bc7aa1edadb1aad61
Rhyan La Rosa <rhyan.larosa.20@usjr.edu.ph>
2022-06-02 11:01:39 +0800

    Hyperparameter continuation

---
+++ init7.py
@@ -0,0 +1,23 @@
+from modules.lib import load_som, load_features
+from modules.services import DatabaseConnection
+from modules.som import tweet_find_cluster
+
+db = DatabaseConnection('mongodb://localhost:27017')
+
+som = load_som()
+features = load_features()
+
+tup = features['tup']
+som_size = som['size']
+som = som['model']
+
+res = list(db.get_clean_tweets())
+
+for data in res:
+    grams = data['grams']
+    print(len(som))
+    print(len(som[0]))
+    print(len(som[0][0]))
+    (matrix, bmu) = tweet_find_cluster(som, som_size, grams, tup)
+    db.conn['cleaned_tweets'].update_one({'tweet_id': data['tweet_id']}, {
+                                         'cluster': {'row': bmu[0], 'col': bmu[1]}})
